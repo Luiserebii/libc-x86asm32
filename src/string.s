ebdc0b24dfd5d8ee7d5db9d577180f8dc6fbea5d
Luiserebii <luis@serebii.io>
2020-03-09 19:12:16 -0400

    Finished strlen; we should implement a way to test this...

--- src/string.s
+++ src/string.s
@@ -13,7 +13,21 @@ strlen:
 	pushl %ebp
 	movl %esp, %ebp
 
+	# Store char* in %ebx
+	movl 8(%ebp), %ebx
 
+	# Reserve %eax for size
+	movl $0, %eax
+	
+strlen_while_char:
+	cmpl $0, (%ebx)
+	je strlen_while_char_end
+	
+	# Increment %eax and char*
+	incl %eax
+	addl $4, %ebx
+
+strlen_while_char_end:	
 	
 	movl %ebp, %esp
 	popl %ebp
