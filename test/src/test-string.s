002379d762844a859015e2422705ceeb717d2000
Luiserebii <luis@serebii.io>
2020-03-13 21:22:36 +0000

    Attempting to build test with Mint functions

--- test/src/test-string.s
+++ test/src/test-string.s
@@ -1,14 +1,42 @@
 .section .data
-meme:
-	.ascii "OwO\0"
+t_strlen:
+	.ascii "strlen() returns correct length\0"
+str_1:
+	.ascii  "apple\0"
+str_2:
+	.ascii "banana\0"
+str_3:
+	.ascii "meme\0"
 
 .section .text
 .globl _start
 
 _start:
-	pushl $meme
+	pushl $str_1
 	call strlen
 
+	pushl $t_strlen
+	pushl $5
+	pushl %eax
+	call test_assert_equal
+
+	pushl $str_2
+	call strlen
+
+	pushl $t_strlen
+	pushl $6
+	pushl %eax
+	call test_assert_equal
+	
+	pushl $str_3
+	call strlen
+
+	pushl $t_strlen
+	pushl $4
+	pushl %eax
+	call test_assert_equal
+
+	call test_end
 	movl %eax, %ebx
 	movl $1, %eax
 	int $0x80
